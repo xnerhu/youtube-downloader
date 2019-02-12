78758b507c82ce0bd36e9d589dddba467ffb94b6
unknown <xnerhu@gmail.com>
2019-02-12 19:41:44 +0100

    Add basics

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("test");
+#!/usr/bin/env node
+
+import { prompt } from 'inquirer';
+
+const cli = () => {
+  prompt([
+    {
+      type: 'list',
+      name: 'type',
+      message: 'What do you want to download?',
+      choices: ['Video', 'Playlist'],
+    },
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is it called?',
+    },
+  ]);
+};
+
+cli();
