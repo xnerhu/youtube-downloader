3702b6f5a78f0ecd31628d9b5c9fffbaf0956580
unknown <xnerhu@gmail.com>
2019-02-15 23:40:19 +0100

    Update prompts

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,30 @@ const cli = () => {
       type: 'list',
       name: 'type',
       message: 'What do you want to download?',
-      choices: ['Video', 'Playlist'],
+      choices: ['A video', 'A playlist'],
     },
     {
       type: 'input',
-      name: 'name',
-      message: 'What is it called?',
+      name: 'title',
+      message: 'What is the title or the url?',
+    },
+    {
+      type: 'list',
+      name: 'type',
+      message: 'Select format:',
+      choices: ['mp4', 'mp3', 'gif', 'ogg', 'wav'],
+    },
+    {
+      type: 'input',
+      name: 'crop-start',
+      message: 'Start',
+      default: '0:00',
+    },
+    {
+      type: 'input',
+      name: 'crop-end',
+      message: 'End',
+      default: '0:00',
     },
   ]);
 };
