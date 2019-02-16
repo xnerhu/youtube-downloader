a144a02a55cecdb0567995291b1c92d76edb1b90
unknown <xnerhu@gmail.com>
2019-02-16 15:13:08 +0100

    Use enquirer instead of inquirer

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-# yt-utils
+# ytd
 
 Download videos, songs and playlists from YouTube.
 
 ## Features
+
 - [ ] Download videos and crop them
 - [ ] Download playlists
 
 ## Available formats:
+
 - [ ] mp4
 - [ ] mp3
 - [ ] gif
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,40 @@
 #!/usr/bin/env node
 
-import { prompt } from 'inquirer';
+import { prompt } from 'enquirer';
+import * as ytdl from 'ytdl-core';
+import * as fs from 'fs';
 
-const cli = () => {
-  prompt([
-    {
-      type: 'list',
-      name: 'type',
-      message: 'What do you want to download?',
-      choices: ['A video', 'A playlist'],
-    },
+const cli = async () => {
+  const { url, quality, format, begin } = await prompt([
     {
       type: 'input',
-      name: 'title',
-      message: 'What is the title or the url?',
+      name: 'url',
+      message: "What's the url?",
     },
     {
-      type: 'list',
-      name: 'type',
-      message: 'Select format:',
+      type: 'autocomplete',
+      name: 'quality',
+      message: 'Select quality',
+      choices: ['highest', 'lowest'],
+      initial: 'highest',
+    },
+    {
+      type: 'autocomplete',
+      name: 'format',
+      message: 'Select format',
       choices: ['mp4', 'mp3', 'gif', 'ogg', 'wav'],
+      initial: 'mp4',
     },
     {
       type: 'input',
-      name: 'crop-start',
-      message: 'Start',
-      default: '0:00',
-    },
-    {
-      type: 'input',
-      name: 'crop-end',
-      message: 'End',
-      default: '0:00',
+      name: 'begin',
+      message: 'Begin',
+      initial: '0:00',
     },
   ]);
+
+  // TODO
+  ytdl(url, { quality, begin }).pipe(fs.createWriteStream(`video.mp4`));
 };
 
 cli();
